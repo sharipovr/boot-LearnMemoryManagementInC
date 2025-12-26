bf63bb8cc164abeb9471b21cc484319306c28b39
sharipovr <sharipov.rustem@gmail.com>
2025-12-26 14:42:36 -0600

    Progressing around half of the chapter in CH1_C_Basics

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Starting the Sneklang interpreter...\n");
-    return 0;
+int max_sneklang_memory(int max_threads, int memory_per_thread) {
+  return max_threads * memory_per_thread; 
+}
+
+// don't touch below this line
+
+void init_sneklang(int max_threads, int memory_per_thread) {
+  printf("Initializing Sneklang\n");
+  printf("Max threads: %d\n", max_threads);
+  printf("Memory per thread: %d\n", memory_per_thread);
+  int max_memory = max_sneklang_memory(max_threads, memory_per_thread);
+  printf("Max memory: %d\n", max_memory);
+  printf("====================================\n");
+}
+
+int main() {
+  init_sneklang(4, 512);
+  init_sneklang(8, 1024);
+  init_sneklang(16, 2048);
+  return 0;
 }
